ab6d32b5e7a08d720a0a0b9908008f3b7ea4bb50
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-12-26 11:09:34 +0800

    c

--- starter/fftw/poisson_py/poisson.py
+++ starter/fftw/poisson_py/poisson.py
@@ -4,7 +4,7 @@ from numpy.fft import fft2, ifft2, fftfreq
 
 def fourier_poisson_solver():
     # 2d poisson equation
-    N = 32                    
+    N = 4                    
     L = 1          
     x = np.linspace(0, L, N, endpoint=False)  
     y = x.copy()
@@ -12,13 +12,23 @@ def fourier_poisson_solver():
     
     # u(x,y) = sin(2*pi*x)sin(2*pi*y)
     u_exact = np.sin(2*np.pi*X) * np.sin(2*np.pi*Y)
-    f = -8 * np.pi * np.pi * np.sin(2*np.pi*X) * np.sin(2*np.pi*Y)
+    #f = -8.0 * np.pi * np.pi * np.sin(2.0*np.pi*X) * np.sin(2.0*np.pi*Y)
+    f = np.array([[-0,-0,-0,0],
+                  [-0,-78.957,-9.6694e-15,78.957],
+                  [-0,-9.6694e-15,-1.1842e-30,9.6694e-15],
+                  [0,78.957,9.6694e-15,-78.957]], dtype=float)
+
+ 
+ 
+
     
     u_numerical = solve_poisson_spectral(f, L)
 
+    return
     error = np.max(np.abs(u_numerical - u_exact))
     print(f"error: {error:.2e}")
-    
+
+    return
     # visualization
     fig, axes = plt.subplots(1, 3, figsize=(15, 4))
     
@@ -63,8 +73,30 @@ def solve_poisson_spectral(f, L):
     K2 = KX**2 + KY**2
     
     K2[0, 0] = 1.0
+
+    print(f)
+    print(f"\n")
     
-    f_hat = fft2(f)
+    #f_hat = fft2(f)
+    #print(f_hat)
+    #print("\n")
+    f_hat = np.fft.fft2(f)
+    print(f_hat)
+    print("\n")
+    
+    F_manual = np.zeros((M, N), dtype=complex)
+    
+    for u in range(M):      # 对应k1
+        for v in range(N):  # 对应k2
+            total = 0 + 0j
+            for x in range(M):      # 对应j1
+                for y in range(N):  # 对应j2
+                    angle = -2 * np.pi * (u*x/M + v*y/N)
+                    total += f[x, y] * (np.cos(angle) + 1j*np.sin(angle))
+            F_manual[u, v] = total
+    print(F_manual)
+
+    #print(np.fft.fftfreq(N, 1/N))
     
     u_hat = -f_hat / K2
     
