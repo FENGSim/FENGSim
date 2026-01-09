bb6d9526515eeec7556bfc356c471dadcbbabf47
fengsim <jiping.xin@lsec.cc.ac.cn>
2026-01-09 15:30:41 +0800

    c

--- starter/karamelo/ex_2/export2vtk.py
+++ starter/karamelo/ex_2/export2vtk.py
@@ -18,34 +18,24 @@ while i < len(files_sorted) :
     z = data[:, 5]
 
     f = open('./dump_p_'+str(i)+'.vtk', 'w')
-    """
+    
     f.write("# vtk DataFile Version 2.0\n")
     f.write("mpm example\n")
     f.write("ASCII\n")
     f.write("DATASET POLYDATA\n")
     f.write("POINTS "+str(len(x))+" float\n")
-    """
+
     j = 0
     while j < len(x) :
         f.write(str(x[j]) + " " + str(y[j]) + " " + str(z[j]) + "\n")
         j = j+1
         
-    """
     f.write("VERTICES " + str(len(x)) + " " + str(2*len(x)) + "\n")
     j = 0
     while j < len(x) :
         f.write("1 " + str(j) + "\n")
         j = j+1
-    
-    f.write("POINT_DATA "+str(len(id))+"\n")
-    f.write("SCALARS group_id int 1\n")
-    f.write("LOOKUP_TABLE default\n")
-    j = 0
-    while j < len(id) :
-        f.write(str(int(id[j])) + "\n")
-        j = j+1
-    """
-        
+
     i = i+1
     
 files = glob.glob(os.path.join("./", "dump_g.*.LAMMPS"))
@@ -63,32 +53,22 @@ while i < len(files_sorted) :
     z = data[:, 5]
 
     f = open('./dump_g_'+str(i)+'.vtk', 'w')
-    """
+
     f.write("# vtk DataFile Version 2.0\n")
     f.write("mpm example\n")
     f.write("ASCII\n")
     f.write("DATASET POLYDATA\n")
     f.write("POINTS "+str(len(x))+" float\n")
-    """
+
     j = 0
     while j < len(x) :
         f.write(str(x[j]) + " " + str(y[j]) + " " + str(z[j]) + "\n")
         j = j+1
         
-    """
     f.write("VERTICES " + str(len(x)) + " " + str(2*len(x)) + "\n")
     j = 0
     while j < len(x) :
         f.write("1 " + str(j) + "\n")
         j = j+1
-    
-    f.write("POINT_DATA "+str(len(id))+"\n")
-    f.write("SCALARS group_id int 1\n")
-    f.write("LOOKUP_TABLE default\n")
-    j = 0
-    while j < len(id) :
-        f.write(str(int(id[j])) + "\n")
-        j = j+1
-    """
-        
+            
     i = i+1
