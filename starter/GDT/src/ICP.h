224937c0954c34a7b5639048e73e59b8e8857d62
fengsim <jiping.xin@lsec.cc.ac.cn>
2026-06-12 11:18:37 +0800

    c

--- starter/GDT/src/ICP.h
+++ starter/GDT/src/ICP.h
@@ -1,39 +1,19 @@
 /*!
+  The codes are from the following examples.
   https://pcl.readthedocs.io/projects/tutorials/en/master/iterative_closest_point.html#iterative-closest-point
   FENGSim/toolkit/Geometry/pcl/doc/tutorials/content/sources/registration_api/example2.cpp
 */
 
 #include <pcl/point_types.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/registration/registration.h>
-#include <pcl/registration/icp.h>
-#include <pcl/registration/joint_icp.h>
-#include <pcl/registration/icp_nl.h>
-#include <pcl/registration/gicp.h>
-#include <pcl/registration/gicp6d.h>
-#include <pcl/registration/transformation_estimation_point_to_plane.h>
-#include <pcl/registration/transformation_validation_euclidean.h>
-#include <pcl/registration/correspondence_rejection_median_distance.h>
-#include <pcl/registration/correspondence_rejection_sample_consensus.h>
-#include <pcl/registration/correspondence_rejection_surface_normal.h>
-#include <pcl/registration/correspondence_estimation_normal_shooting.h>
-#include <pcl/registration/pyramid_feature_matching.h>
-#include <pcl/features/ppf.h>
-#include <pcl/registration/ppf_registration.h>
-#include <pcl/filters/voxel_grid.h>
-// We need Histogram<2> to function, so we'll explicitly add kdtree_flann.hpp here
-#include <pcl/kdtree/impl/kdtree_flann.hpp>
-//(pcl::Histogram<2>)
 
 using namespace pcl;
 using namespace pcl::io;
 
-class icp_align {
- public:
+class ICP {
+public:
     Eigen::Matrix4f transform;
     Eigen::Matrix4f inverse_transform;
-    double align (PointCloud<PointXYZ>& cloud_source, PointCloud<PointXYZ>& cloud_target, PointCloud<PointXYZ>& cloud_icp);
-    void align_back (PointCloud<PointXYZ>& cloud_source, PointCloud<PointXYZ>& cloud_target, PointCloud<PointXYZ>& cloud_icp);
+    double Align (PointCloud<PointXYZ>& cloud_source, PointCloud<PointXYZ>& cloud_target, PointCloud<PointXYZ>& cloud_icp);
+    void AlignBack (PointCloud<PointXYZ>& cloud_source, PointCloud<PointXYZ>& cloud_target, PointCloud<PointXYZ>& cloud_icp);
 };
 
--- starter/GDT/src/SACIA.h
+++ starter/GDT/src/SACIA.h
@@ -1,24 +1,16 @@
 /*!
-  the codes from FENGSim/toolkit/Geometry/pcl/test/registration/test_sac_ia.cpp
+  The codes are from the following examples.
+  FENGSim/toolkit/Geometry/pcl/test/registration/test_sac_ia.cpp.
 */
 
-#include <pcl/point_types.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/features/fpfh.h>
-#include <pcl/registration/ia_ransac.h>
-#include <pcl/registration/sample_consensus_prerejective.h>
-#include <pcl/features/pfh.h>
-#include <pcl/features/normal_3d_omp.h>
-#include "Tools.h"
-
+#include "pcl/point_types.h"
 
 using namespace pcl;
 using namespace pcl::io;
 
-class sacia_align {
- public:
+class SACIA {
+public:
     Eigen::Matrix4f transform;
-    double align (PointCloud<PointXYZ>& cloud_source, PointCloud<PointXYZ>& cloud_target,
+    double Align (PointCloud<PointXYZ>& cloud_source, PointCloud<PointXYZ>& cloud_target,
 		  PointCloud<PointXYZ>& cloud_sacia, double radius=1, double itnum=2000);
 };
